1e27fde1f3a2f2061b9b418c091ae87bd5366da3
JoJo <jbuss174@gmail.com>
2022-05-23 01:55:32 +0200

    Save State
    nothing rlly changed just a Save State

--- FM Wirf die Flinte nicht ins Korn/Assets/Scripts/GeneralSettings.cs
+++ FM Wirf die Flinte nicht ins Korn/Assets/Scripts/GeneralSettings.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class GeneralSettings : MonoBehaviour
 {
-    // Start is called before the first frame update
     void Start()
     {
         Application.targetFrameRate = 120;
