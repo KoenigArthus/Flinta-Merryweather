158330c4cf5450bf69ce6b84b5694a16023f2716
JoJo <jbuss174@gmail.com>
2022-06-08 02:37:08 +0200

    State Save

--- FM Wirf die Flinte nicht ins Korn/Assets/Scripts/Character.cs
+++ FM Wirf die Flinte nicht ins Korn/Assets/Scripts/Character.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Character : Interactable
 {
+    //TO DO: Character still needs a Scriptable Object Version
+
+    //this is where the character is given an Ink file
     [SerializeField] private TextAsset ink;
 
     public override void ReactToClick()
