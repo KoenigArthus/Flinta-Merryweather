2e8350f5c2c849a1e0341fbaf1029aeccb1f1c71
JoJo <jbuss174@gmail.com>
2022-06-28 02:33:44 +0200

    organized Scripts

---
+++ FM Wirf die Flinte nicht ins Korn/Assets/Scripts/Special Scripts/NoReturn.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoReturn
+{
+   // This class is only here to indicate that no item should be returned when crafting with this.gameObject
+}
